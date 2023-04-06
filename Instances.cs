55904145d385b8d8dac7eb1b13d6389c171b6e45
Kostya <kosta3suxar@mail.ru>
2023-04-06 07:41:38 +0600

    First commit

---
+++ Instances.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Instances
+    {
+
+      private static EsofitEntities _db=null;
+        public static EsofitEntities getInstances() 
+        {
+
+            if (_db == null) {
+            
+                _db=new EsofitEntities();
+            
+            
+            }
+            return _db;
+
+
+        }
+
+
+    }
+}
